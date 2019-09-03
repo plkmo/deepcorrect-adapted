f6461d142579add104b15139e392df095ada1d18
plkmo <plkmo@hotmail.com>
2019-09-03 14:02:56 +0800

    updated readme

---
+++ README.md
@@ -0,0 +1,45 @@
+# Deepcorrect
+(Adapted from https://github.com/bedapudi6788/deepcorrect)
+
+Given unpunctuated (and perhaps also un-capitalized) text, this module helps to punctuate\capitalize the text accordingly. 
+
+## Model Architecture
+Seq2Seq LSTM
+
+## Pre-trained models available
+0: deeppunct_checkpoint_tatoeba_cornell (trained on tatoeba dataset)
+1: deeppunct_checkpoint_google_news (trained on google news dataset)
+2: deeppunct_checkpoint_wikipedia (trained on Wikipedia dataset)
+
+## Requirements
+Tensorflow=1.14
+
+## Usage
+Git clone repo
+
+```bash
+git clone https://github.com/plkmo/deepcorrect-adapted
+cd ./
+infer.py [-h for pre-trained model selection]
+```
+
+Output:
+```bash
+Input sentence to punctuate:
+i ate ice cream today the weather is good
+09/03/2019 01:49:27 PM [INFO]: Punctuating...
+I ate ice cream today, the weather is good.
+```
+
+```bash
+from infer import load_model
+corrector = load_model('./model_data/deeppunct_checkpoint_tatoeba_cornell')
+sent = 'I have a dog.'
+print(corrector.correct(sent)[0]['sequence'])
+```
+
+Output:
+```bash
+I have a dog.
+```
+
--- infer.py
+++ infer.py
@@ -6,8 +6,41 @@ Created on Tue Sep  3 12:49:48 2019
 """
 
 from deepcorrect import DeepCorrect
+from argparse import ArgumentParser
+import logging
 
-checkpoint_path = "./model_data/deeppunct_checkpoint_tatoeba_cornell"
-params_path = "./model_data/deeppunct_params_en"
-corrector = DeepCorrect(params_path, checkpoint_path)
-corrector.correct('hey')
+logging.basicConfig(format='%(asctime)s [%(levelname)s]: %(message)s', \
+                    datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
+logger = logging.getLogger('__file__')
+
+def load_model(pretrained):
+    logger.info("Loading pre-trained model...")
+    model_dict = {0: 'deeppunct_checkpoint_tatoeba_cornell',\
+                  1: 'deeppunct_checkpoint_google_news',\
+                  2: 'deeppunct_checkpoint_wikipedia'}
+    if pretrained not in model_dict.keys():
+        model = 'deeppunct_checkpoint_tatoeba_cornell'
+    else:
+        model = model_dict[args.model]
+    checkpoint_path = "./model_data/%s" % model
+    params_path = "./model_data/deeppunct_params_en"
+    corrector = DeepCorrect(params_path, checkpoint_path)
+    logger.info("Loaded!")
+    return corrector
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--model", type=int, default=0,\
+                        help="Choose from 3 pre-trained models:\n\
+                        0: deeppunct_checkpoint_tatoeba_cornell\n\
+                        1: deeppunct_checkpoint_google_news\n\
+                        2: deeppunct_checkpoint_wikipedia")
+    args = parser.parse_args()
+    
+    corrector = load_model(args.model)
+    while True:
+        sent = input("Input sentence to punctuate:\n")
+        if sent in ["quit", "exit"]:
+            break
+        logger.info("Punctuating...")
+        print(corrector.correct(sent)[0]['sequence'])
